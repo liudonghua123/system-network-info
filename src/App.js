cf6f2e4b301d22a811e431960d118184e53c8cca
liudonghua123 <liudonghua123@gmail.com>
2018-11-01 15:26:45 +0800

    add system info

--- src/App.js
+++ src/App.js
@@ -12,21 +12,53 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Chip from '@material-ui/core/Chip';
 import Button from '@material-ui/core/Button';
 import Divider from '@material-ui/core/Divider';
+
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+
+import ListSubheader from '@material-ui/core/ListSubheader';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Collapse from '@material-ui/core/Collapse';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import SendIcon from '@material-ui/icons/Send';
+import StarIcon from '@material-ui/icons/Star';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import StarBorder from '@material-ui/icons/StarBorder';
+
+
 import Paper from '@material-ui/core/Paper';
 import logo from './logo.svg';
 import Header from './components/Header';
 import Footer from './components/Footer';
+
 const { remote, ipcRenderer } = window.require('electron');
 const { networkInterfaces, getNetworkInterfaces } = remote.require('os');
+
 ipcRenderer.on('networkInterfaces', (evt, msg) => console.log(`evt: ${evt}, msg: ${msg}`));
+
+const { Resolver } = remote.require('dns');
+const resolver = new Resolver();
+const dnsServers = resolver.getServers();
+
+const si = remote.require('systeminformation');
 
 const styles = theme => ({
   root: {
+    width: '100%',
+  },
+  card: {
     width: '100%',
   },
   heading: {
@@ -56,16 +88,40 @@ class App extends Component {
     super(props);
     this.props = props;
     this.state = {
-      interfaceData: networkInterfaces()
+      interfaceData: networkInterfaces(),
+      si: {
+        os: {},
+        system: {},
+        bios: {},
+        baseboard: {},
+        cpu: {},
+        mem: {}
+      },
+      open: {
+        os: false,
+        system: false,
+        bios: false,
+        baseboard: false,
+        cpu: false,
+        mem: false,
+      }
     };
-    console.info(`constructor this.state.interfaceData: ${JSON.stringify(this.state.interfaceData)}, ${JSON.stringify(remote.process.versions)}`)
-  }
+    console.info(`constructor this.state.interfaceData: ${JSON.stringify(this.state.interfaceData)}, ${JSON.stringify(remote.process.versions)}`);
 
-  componentWillMount() {
-    this.setState({
-      // interfaceData: networkInterfaces()
-    });
-    // console.info(`componentWillMount this.state.interfaceData: ${JSON.stringify(networkInterfaces())}`)
+    (async () => {
+      try {
+        const os= await si.osInfo();
+        const system= await si.system();
+        const bios= await si.bios();
+        const baseboard= await si.baseboard();
+        const cpu= await si.cpu();
+        const mem= await si.mem();
+        console.log(`${JSON.stringify({ si: { os, system, bios, baseboard, cpu, mem }}, null, 2)}`);
+        this.setState({ si: { os, system, bios, baseboard, cpu, mem }})
+      } catch (e) {
+        console.log(e)
+      }
+    })();
   }
 
   createExpansionPanels = (interfaceData) => {
@@ -120,14 +176,142 @@ class App extends Component {
     return expansionPanels;
   }
 
+  handleClick = (item) => (e) => {
+    this.setState(state => ({ open: { [item]: !state.open[item] }}));
+  };
+
   render() {
     const { classes } = this.props;
     return (
       <div className="App">
         <Header />
-        <div className={classes.root}>
+        <Card className={classes.card}>
+          <CardContent>
           {this.createExpansionPanels(this.state.interfaceData)}
-        </div>
+          </CardContent>
+        </Card>
+        <Card className={classes.card}>
+          <CardContent>
+            <List
+              component="nav"
+              subheader={<ListSubheader component="div">System Infomations</ListSubheader>}
+            >
+              <ListItem button >
+                <ListItemIcon>
+                  <StarIcon />
+                </ListItemIcon>
+                <ListItemText inset primary="dns" secondary={ dnsServers.join(", ") } />
+              </ListItem>
+              
+              <ListItem button onClick={this.handleClick('os')}>
+                <ListItemIcon>
+                  <StarIcon />
+                </ListItemIcon>
+                <ListItemText inset primary="os" secondary={ this.state.si.os.distro } />
+                {this.state.open.os ? <ExpandLess /> : <ExpandMore />}
+              </ListItem>
+              <Collapse in={this.state.open.os} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                  <ListItem button className={classes.nested}>
+                    <ListItemText inset primary={ "platform" } secondary={ this.state.si.os.platform } />
+                  </ListItem>
+                  <ListItem button className={classes.nested}>
+                    <ListItemText inset primary={ "arch" } secondary={ this.state.si.os.arch } />
+                  </ListItem>
+                  <ListItem button className={classes.nested}>
+                    <ListItemText inset primary={ "distro" } secondary={ this.state.si.os.distro } />
+                  </ListItem>
+                  <ListItem button className={classes.nested}>
+                    <ListItemText inset primary={ "kernel" } secondary={ this.state.si.os.kernel } />
+                  </ListItem>
+                </List>
+              </Collapse>
+              
+              <ListItem button onClick={this.handleClick('system')}>
+                <ListItemIcon>
+                  <StarIcon />
+                </ListItemIcon>
+                <ListItemText inset primary="system" secondary={ this.state.si.system.manufacturer } />
+                {this.state.open.system ? <ExpandLess /> : <ExpandMore />}
+              </ListItem>
+              <Collapse in={this.state.open.system} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                  <ListItem button className={classes.nested}>
+                    <ListItemText inset primary={ "manufacturer" } secondary={ this.state.si.system.manufacturer } />
+                  </ListItem>
+                  <ListItem button className={classes.nested}>
+                    <ListItemText inset primary={ "model" } secondary={ this.state.si.system.model } />
+                  </ListItem>
+                  <ListItem button className={classes.nested}>
+                    <ListItemText inset primary={ "serial" } secondary={ this.state.si.system.serial } />
+                  </ListItem>
+                </List>
+              </Collapse>
+              <ListItem button onClick={this.handleClick('cpu')}>
+                <ListItemIcon>
+                  <StarIcon />
+                </ListItemIcon>
+                <ListItemText inset primary="cpu" secondary={ this.state.si.cpu.manufacturer } />
+                {this.state.open.cpu ? <ExpandLess /> : <ExpandMore />}
+              </ListItem>
+              <Collapse in={this.state.open.cpu} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                  <ListItem button className={classes.nested}>
+                    <ListItemText inset primary={ "manufacturer" } secondary={ this.state.si.cpu.manufacturer } />
+                  </ListItem>
+                  <ListItem button className={classes.nested}>
+                    <ListItemText inset primary={ "brand" } secondary={ this.state.si.cpu.brand } />
+                  </ListItem>
+                  <ListItem button className={classes.nested}>
+                    <ListItemText inset primary={ "speed" } secondary={ this.state.si.cpu.speed } />
+                  </ListItem>
+                </List>
+              </Collapse>
+
+              <ListItem button onClick={this.handleClick('mem')}>
+                <ListItemIcon>
+                  <StarIcon />
+                </ListItemIcon>
+                <ListItemText inset primary="mem" secondary={ parseInt(this.state.si.mem.free / 1048576) + " MB Free" } />
+                {this.state.open.mem ? <ExpandLess /> : <ExpandMore />}
+              </ListItem>
+              <Collapse in={this.state.open.mem} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                  <ListItem button className={classes.nested}>
+                    <ListItemText inset primary={ "total" } secondary={ parseInt(this.state.si.mem.total / 1048576) + " MB"  } />
+                  </ListItem>
+                  <ListItem button className={classes.nested}>
+                    <ListItemText inset primary={ "free" } secondary={ parseInt(this.state.si.mem.free / 1048576) + " MB" } />
+                  </ListItem>
+                  <ListItem button className={classes.nested}>
+                    <ListItemText inset primary={ "used" } secondary={ parseInt(this.state.si.mem.used / 1048576) + " MB" } />
+                  </ListItem>
+                </List>
+              </Collapse>
+              
+              <ListItem button onClick={this.handleClick('baseboard')}>
+                <ListItemIcon>
+                  <StarIcon />
+                </ListItemIcon>
+                <ListItemText inset primary="baseboard" secondary={ this.state.si.baseboard.manufacturer } />
+                {this.state.open.baseboard ? <ExpandLess /> : <ExpandMore />}
+              </ListItem>
+              <Collapse in={this.state.open.baseboard} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                  <ListItem button className={classes.nested}>
+                    <ListItemText inset primary={ "manufacturer" } secondary={ this.state.si.baseboard.manufacturer } />
+                  </ListItem>
+                  <ListItem button className={classes.nested}>
+                    <ListItemText inset primary={ "model" } secondary={ this.state.si.baseboard.model } />
+                  </ListItem>
+                  <ListItem button className={classes.nested}>
+                    <ListItemText inset primary={ "serial" } secondary={ this.state.si.baseboard.serial } />
+                  </ListItem>
+                </List>
+              </Collapse>
+            </List>
+          </CardContent>
+        </Card>
         <Footer />
       </div>
     );
